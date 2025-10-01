2273f89bf9057fd060646db007c445a63f7ebc11
Martin Grigoryan <grigoryanmartin20@gmail.com>
2025-10-01 11:13:45 +0400

    Initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+// Routes
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideZonelessChangeDetection(),
+		provideRouter(routes)
+	]
+};
